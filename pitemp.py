a3268c25842e7f57a2f815de6d305a6914997b08
Ryan Adzima <ryan@ronintech.io>
2016-04-02 09:06:41 +0000

    Added live monitor

--- pitemp.py
+++ pitemp.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python
 
 from subprocess import Popen,PIPE
-import sys,argparse
+import time,sys,argparse
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
 parser = argparse.ArgumentParser(description="Get the Raspberry Pi's temperature")
+parser.add_argument("-l","--live",
+	action="store_true",
+	help="Monitor the temperature live.")
+parser.add_argument("-i","--interval",
+	action="store",
+	default=5,
+	type=int,
+	help="Refresh interval for the live view.")
 group = parser.add_mutually_exclusive_group()
 group.add_argument("-c","--celsius",
 	action="store_true",
@@ -35,13 +43,27 @@ def main():
 	tempString = getTempString()
 	celsius = convertToC(tempString)
 	fahrenheit = celsiusToF(celsius)
-	if args.fahrenheit:
-		print "{:.2f}".format(fahrenheit)
+	if args.live:
+		print "|\tCelsius\t\t|\tFahrenheit\t|"
+		while True:
+			try:
+				tempString = getTempString()
+				celsius = convertToC(tempString)
+				fahrenheit = celsiusToF(celsius)
+				print "\t{0:.2f}{1}C\t\t\t{2:.2f}{3}F\r".format(celsius,u"\u00B0",fahrenheit,u"\u00B0"),
+				sys.stdout.flush()
+				time.sleep(args.interval)
+				print "\r",
+			except (KeyboardInterrupt, SystemExit):
+				print "Exiting..."
+				raise
+	elif args.fahrenheit:
+		print "Temperature:\t{:.2f}".format(fahrenheit)
 	elif args.celsius:
-		print "{:.2f}".format(celsius)
+		print "Temperature:\t{:.2f}".format(celsius)
 	else:
-		print "{:.2f}{}C".format(celsius,u"\u00B0")
-		print "{:.2f}{}F".format(fahrenheit,u"\u00B0")
+		print "Celsius:\t{:.2f}{}C".format(celsius,u"\u00B0")
+		print "Fahrenheit:\t{:.2f}{}F".format(fahrenheit,u"\u00B0")
 
 if __name__ == "__main__":
 	main()
